77631f2b4cbe5ff425575165da9dd8edd170514b
Taylor Conn <taylor.conn.ctr@nps.edu>
2025-07-03 20:44:49 -0700

    removed token .env debug comment

--- job_hunt_bot.py
+++ job_hunt_bot.py
@@ -13,9 +13,6 @@ load_dotenv()
 TOKEN =  os.getenv("DISCORD_BOT_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", "0"))
 
-print(f"[DEBUG] Token starts with: {TOKEN[:5]}")
-
-
 intents = discord.Intents.default()
 intents.message_content = True
 from discord.ext import commands
